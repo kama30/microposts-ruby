0e96a4f07beb34e85efbca04ca113e52faea8e1d
saori <kamachaso.30@gmail.com>
2019-05-12 13:36:08 +0000

    add likes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :reserves_of_favorites, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :users, through: :reserves_of_favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reserves_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reserves_of_relationships, source: :user
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -30,4 +32,21 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorited?(micropost)
+    self.favorite_microposts.include?(micropost)
+  end
+  
+  # def favorites
+  #   Micropost.where(user_id: self.favorite_ids)
+  # end
 end
